d8a40f7102acb216826c1c3f29be32fb0914a54d
hieunv <toiyeuthethao1997@gmail.com>
2018-10-16 20:18:23 +0700

    display images in RecyclerView

--- app/src/main/java/com/cris/nvh/recyclerview/MainActivity.java
+++ app/src/main/java/com/cris/nvh/recyclerview/MainActivity.java
@@ -2,6 +2,8 @@ package com.cris.nvh.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+		RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+		RecyclerView.Adapter adapter = new HeroAdapter();
+		recyclerView.setLayoutManager(layoutManager);
+		recyclerView.setAdapter(adapter);
 	}
 }
